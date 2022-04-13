0bd26c18291d96112516b196b0e6ce5a1407c25c
Phillip Ho <philasan@gmail.com>
2022-04-12 19:45:48 -0700

    start making game

---
+++ components/Game/index.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+const Game = () => {
+
+  // const maxTurns = 10
+
+  // secret code in state
+
+  // generateSecretCode()
+    // returns 4 different digits, 0-7,
+
+  // guesses in state
+
+  // calculateFeedback(guess)
+
+  // handleGuess(guess)
+    // add guess to guesses
+    // compare guess to secret code
+
+
+  // evaluate for end condition after every guess
+    // either guess matches secret code or player has run out of turns
+
+  // turnsLeft () => maxTurns - guesses
+
+  // reset/restart game
+    // generate new code
+    // clear guesses
+
+  return (
+    <div>
+      GAME
+    </div>
+  )
+
+};
+
+export default Game;
