6190da8d91f5bfd1195b8a30a6d7fd3652eb4e7c
Phillip Ho <philasan@gmail.com>
2022-04-13 23:16:55 -0700

    computer can provide feedback

--- components/Game/index.js
+++ components/Game/index.js
@@ -8,9 +8,37 @@ const Game = () => {
   const maxTurns = 10;
   const codes = ['0', '1', '2', '3', '4', '5', '6', '7'];
 
-  const generateFeedback = (guess) => {
-    // return feedback for guess
+  const createFeedback = (guess) => {
+    let fullMatch = 0;
+    let partialMatch = 0;
+
+    const guessCopy = [...guess];
+    const secretCopy = [...secretCode];
+
+    // look for full matches
+    for (let i = 0; i < secretCode.length; i++) {
+      if (guess[i] === secretCode[i]) {
+        fullMatch++;
+        guessCopy.splice(i, 1);
+        secretCopy.splice(i, 1);
+      }
+    }
+
+    // look for partial matches
+    for (let guessCode of guessCopy) {
+      const i = secretCopy.indexOf(guessCode);
+      if (i > 0) {
+        partialMatch++;
+        secretCopy.splice(i, 1);
+      }
+    }
+
+    return {
+      fullMatch,
+      partialMatch,
+    };
   }
+
   const generateSecretCode = () => {
     return ['1', '2', '3', '4'];
   }
@@ -40,7 +68,9 @@ const Game = () => {
     <div className={styles.game}>
       {hasWon() && (<div className={styles.hasWon}>You Won!</div>)}
       {hasLost() && (<div className={styles.hasWon}>You Lost!</div>)}
-      <GuessList guesses={guesses} />
+      <GuessList
+        guesses={guesses}
+        feedbacks={guesses.map((guess) => createFeedback(guess))} />
       <CodeMaker
         size={4}
         codes={codes}
